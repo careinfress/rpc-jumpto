f3d23868a06c33a46160c78a63b395dcd936aff2
careinfress <814207100@qq.com>
2019-01-24 14:52:39 +0800

    FaiCliLineMarker测试成功在cli中标记 build.gradle默认使用UTF-8编码

--- src/main/java/com/fkw/rpc/provider/FaiCliLineMarkerProvider.java
+++ src/main/java/com/fkw/rpc/provider/FaiCliLineMarkerProvider.java
@@ -9,62 +9,88 @@ import com.intellij.psi.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.Objects;
 
 public class FaiCliLineMarkerProvider extends RelatedItemLineMarkerProvider {
 
     private final String FAI_CLI_PREFIX = "fai.cli";
     private final String FAI_CLI_EXPRESSION = "sendProtocol.setCmd";
 
+
+    /**
+     * 该方法的实现是从目标对象反向向上校验
+     * 不能跟我们的逻辑上下手从范围大的扫描到小的
+     * @param element
+     * @param result
+     */
     @Override
     protected void collectNavigationMarkers(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo> result) {
-        //判断是否为class元素
-        if (!(element instanceof PsiClass)) return;
-        //判断包名是否符合规定
-        if (!isTargetField(element)) return;
 
-        PsiClass psiClass = (PsiClass) element;
-        //拿到该类的所有的所有方法
-        PsiMethod[] allMethods = psiClass.getAllMethods();
-        for (PsiMethod method : allMethods) {
-            //获取最每个方法最后的代码块
-            PsiElement lastChild = method.getLastChild();
-            if (!(lastChild instanceof PsiCodeBlock)) return;
-            PsiCodeBlock psiCodeBlock = (PsiCodeBlock) lastChild;
-            PsiElement[] children = psiCodeBlock.getChildren();
-            for (PsiElement child : children) {
-                //如果包含try代码块
-                if ((child instanceof PsiTryStatement)) {
-                    PsiTryStatement PsiTryStatement = (PsiTryStatement)child;
-                    PsiCodeBlock tryBlock = PsiTryStatement.getTryBlock();
-                    if (tryBlock == null) {
-                        continue;
-                    }
-                    //拿到try代码块中指定变量
-                    PsiStatement[] tryBlockStatements = tryBlock.getStatements();
-                    for (PsiStatement tryBlockStatement : tryBlockStatements) {
-                        if (tryBlockStatement.getText().equals(FAI_CLI_EXPRESSION)) {
-                            //可以注上标记
-                            NavigationGutterIconBuilder<PsiElement> builder =
-                                    NavigationGutterIconBuilder.create(Icons.SPRING_INJECTION_ICON)
-                                            .setAlignment(GutterIconRenderer.Alignment.CENTER)
-                                            .setTarget(psiClass)
-                                            .setTooltipTitle("Data access object found - " + psiClass.getQualifiedName());
-                            result.add(builder.createLineMarkerInfo(((PsiNameIdentifierOwner) element).getNameIdentifier()));
-                        }
-                    }
-                }
+        //判断是否为方法
+        if (!(element instanceof PsiMethod)) return;
+
+        //方法的父类为class
+        PsiElement psiClazz = element.getParent();//PsiClass
+        if (psiClazz == null || !(element instanceof PsiClass)) return;
+        PsiClass psiClass = (PsiClass) psiClazz;
+        //判断是否在fai.cli包下
+        if (!Objects.requireNonNull(psiClass.getQualifiedName()).startsWith(FAI_CLI_PREFIX)) return;
+
+        PsiMethod psiMethod = (PsiMethod) element;
+        PsiCodeBlock psiCodeBlock = psiMethod.getBody();
+        if (psiCodeBlock == null) return;
+        PsiStatement[] statements = psiCodeBlock.getStatements();
+        for (PsiStatement statement : statements) {
+            System.out.println(statement.getText());
+            System.out.println("====================");
+            if (statement.getText().contains(FAI_CLI_EXPRESSION)) {
+                NavigationGutterIconBuilder<PsiElement> builder =
+                NavigationGutterIconBuilder.create(Icons.SPRING_INJECTION_ICON)
+                        .setAlignment(GutterIconRenderer.Alignment.CENTER)
+                        .setTarget(element)
+                        .setTooltipTitle("Data access object found - ");
+                result.add(builder.createLineMarkerInfo(element));
             }
         }
 
+
+//        //判断有无try代码块
+//        if (!(element instanceof PsiTryStatement)) return;
+//        PsiTryStatement psiTryStatement = (PsiTryStatement) element;
+//        PsiCodeBlock tryBlock = psiTryStatement.getTryBlock();
+//
+//        if (tryBlock == null) return;
+//        if (tryBlock.getText().contains(FAI_CLI_EXPRESSION)) {
+//            System.out.println("这里进来的都是方法中的代码块");
+//            NavigationGutterIconBuilder<PsiElement> builder =
+//                    NavigationGutterIconBuilder.create(Icons.SPRING_INJECTION_ICON)
+//                            .setAlignment(GutterIconRenderer.Alignment.CENTER)
+//                            .setTarget(element)
+//                            .setTooltipTitle("Data access object found - ");
+//            result.add(builder.createLineMarkerInfo(element));
+//        }
+
+
+
+//        //判断是否为Psi表达式代码块
+//        if (!(element instanceof PsiMethodCallExpression)) return;
+//
+//
+//        PsiMethodCallExpression psiMethodCallExpression = (PsiMethodCallExpression) element;
+//        PsiFile psiFile = psiMethodCallExpression.getContainingFile();
+//
+//
+//
+//        System.out.println(psiMethodCallExpression.getText());
+//        if (!psiMethodCallExpression.getText().startsWith(FAI_CLI_EXPRESSION)) return;
+//
+//        NavigationGutterIconBuilder<PsiElement> builder =
+//                NavigationGutterIconBuilder.create(Icons.SPRING_INJECTION_ICON)
+//                        .setAlignment(GutterIconRenderer.Alignment.CENTER)
+//                        .setTarget(element)
+//                        .setTooltipTitle("Data access object found - ");
+//        result.add(builder.createLineMarkerInfo(((PsiNameIdentifierOwner) element).getNameIdentifier()));
+
     }
-
-
-
-    private boolean isTargetField(@NotNull PsiElement element) {
-        //cli类的包名是固定的,都是在fai.cli下
-        PsiPackageStatement psiPackageStatement =  (PsiPackageStatement) element;
-        return psiPackageStatement.getPackageName().equals(FAI_CLI_PREFIX);
-    }
-
 
 }
